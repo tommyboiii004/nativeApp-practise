d1bc17fd3bd9d1df80438c5d5471671c7b99349f
tommyboiii004 <thomastharun004@gmail.com>
2024-07-02 21:17:19 +0530

    setup complete with fontLoaded

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Index</Text>
+      <Text className="text-3xl font-pblack">Index</Text>
       <StatusBar style="auto" />
     </View>
   );
