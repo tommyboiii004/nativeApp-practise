afc764d6b64884f2fd1a6e0819574a165ba9186d
tommyboiii004 <thomastharun004@gmail.com>
2024-07-08 11:10:46 +0530

    finished bottom tab fully

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Index</Text>
+      <Text className="text-3xl font-pblack">Aora</Text>
       <StatusBar style="auto" />
       <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
     </View>
