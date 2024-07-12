e7b271f5bd49dba4079bbf38f5828efe4a5ae8b5
tommyboiii004 <thomastharun004@gmail.com>
2024-07-12 16:39:59 +0530

    file name changes

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import { getCurrentUser } from "../../lib/appwrite";
+import { getCurrentUser } from '../lib/appwrite';
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => {
