012a4f006607fad4242ea0a55a74489a833cf2b0
tommyboiii004 <thomastharun004@gmail.com>
2024-07-24 15:39:05 +0530

    profile page completed except logout

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,75 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, FlatList, TouchableOpacity, Image} from 'react-native'
+import {React, useEffect} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import EmptyState from '../../components/EmptyState'
+import {getUserPosts} from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
+import { icons } from '../../constants'
+import InfoBox from '../../components/InfoBox'
 
 const Profile = () => {
+
+  const { user, setUser, setIsLogged } = useGlobalContext();
+  const { data: posts } = useAppwrite(() => getUserPosts(user.$id));
+
+  const logout = () => {
+    
+  }
+
   return (
-    <View className="items-center justify-center">
-      <Text className="text-3xl font-pblack">Profile</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard video={ item } /> 
+        )}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb -12 px-4">
+            <TouchableOpacity className='w-full items-end mb-10' onPress={logout}>
+              <Image
+                source={icons.logout} resizeMode='contain' className='w-6 h-6'
+              />
+            </TouchableOpacity>
+            <View className='w-16 h-16 border-secondary rounded-lg justify-center items-center'>
+              <Image source={{ uri : user?.avatar}} className='w-[90%] h-[90%] rounded-lg' resizeMode='cover'/>
+            </View>
+
+            <InfoBox
+              title={user?.username}
+              containerStyles='mt-5'
+              titleStyles='text-lg' 
+            />
+
+            <View className='mt-5 flex-row mb-16'>
+              <InfoBox
+                title={posts.length || 0}
+                subtitle="Posts"
+                containerStyles='mr-10'
+                titleStyles='text-xl' 
+              />
+              
+              <InfoBox
+              title='10k'
+              subtitle='Followers'
+              titleStyles='text-xl' 
+              />
+            </View>
+
+          </View>
+
+
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="The user has not uploaded any videos yet"
+          />
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,5 +1,5 @@
 import { View, Text, FlatList} from 'react-native'
-import {React, useState, useEffect} from 'react'
+import {React, useEffect} from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SearchInput from '../../components/SearchInput'
 import EmptyState from '../../components/EmptyState'
